906b2ba4bfc589f362eae8614a76bf2d931bc4c0
juan-20 <juanalvesandrade@gmail.com>
2022-06-21 08:54:07 -0300

    setting header and home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,15 @@
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <a>
+          <img src="https://avatars.githubusercontent.com/u/49418565?v=4" alt="logo" />
+        </a>
+      </Link>
+    </header>
+  );
 }
